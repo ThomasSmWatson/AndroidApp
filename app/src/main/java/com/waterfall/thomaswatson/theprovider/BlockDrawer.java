7e298b5be42618e0065a7e2d84f1a8ae134816d0
Thomas Watson <thomas.sm.watson@gmail.com>
2015-07-17 13:49:45 +0100

    -partFIX need ot be fixed TOUCH EVENT

--- app/src/main/java/com/waterfall/thomaswatson/theprovider/BlockDrawer.java
+++ app/src/main/java/com/waterfall/thomaswatson/theprovider/BlockDrawer.java
@@ -5,6 +5,8 @@ import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Point;
 import android.view.Display;
+import android.view.MotionEvent;
+import android.view.VelocityTracker;
 import android.view.View;
 import android.view.WindowManager;
 
@@ -24,7 +26,10 @@ public class BlockDrawer extends View{
     Display display;
     Point size;
     Position<Integer> screenCenter;
+    int offsetX, offsetY = 0;
+    Position<Integer> movement = new Position<Integer>();
 
+    private VelocityTracker mVelocityTracker = null;
     public BlockDrawer(Context context, ArrayList<Block> blocks) {
         super(context);
         display = ((WindowManager) getContext().getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay();
@@ -35,6 +40,7 @@ public class BlockDrawer extends View{
         display.getSize(size);
         int width = size.x;
         int height = size.y;
+
         screenCenter = new Position<Integer>();
         screenCenter.setX(width/2);
         screenCenter.setY(height/2);
@@ -51,8 +57,8 @@ public class BlockDrawer extends View{
         for(Block block: blocks) {
 
             Position<Integer> position = new Position<Integer>();
-            position.setX(block.getPosition().getX() * GrassBlock.getScale() - getCenter().getX() + screenCenter.getX());
-            position.setY(block.getPosition().getY() * GrassBlock.getScale()- getCenter().getY() + screenCenter.getY());
+            position.setX(block.getPosition().getX() * GrassBlock.getScale() - getCenter().getX() + screenCenter.getX() + offsetX);
+            position.setY(block.getPosition().getY() * GrassBlock.getScale() - getCenter().getY() + screenCenter.getY() + offsetY);
 
             canvas.drawBitmap(block.getBlockImage(), position.getX(), position.getY() , null);
 
@@ -60,12 +66,17 @@ public class BlockDrawer extends View{
         }
     }
 
+
+
+
     public Position<Integer> getCenter(){
         Position<Integer> center = new Position<Integer>();
         center.setX( 15 * Block.getScale()+Block.getScale()/2);
-        center.setY( 15 *Block.getScale()+Block.getScale()/2);
+        center.setY(15 * Block.getScale() + Block.getScale() / 2);
         return center;
     }
+
+
 
 
 
--- app/src/main/java/com/waterfall/thomaswatson/theprovider/Game.java
+++ app/src/main/java/com/waterfall/thomaswatson/theprovider/Game.java
@@ -2,6 +2,7 @@ package com.waterfall.thomaswatson.theprovider;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 
 import com.waterfall.thomaswatson.theprovider.util.SystemUiHider;
 
@@ -16,6 +17,8 @@ public class Game extends Activity {
 
     PlayerArea area;
     BlockDrawer drawer;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,5 +43,28 @@ public class Game extends Activity {
 //
 //        area.drawPlayerArea(this.getApplicationContext());
     }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+
+        int action = event.getActionMasked();
+        Position<Integer> touchPosition = new Position<Integer>();
+
+        if(action == MotionEvent.ACTION_DOWN){
+            touchPosition.setX((int) event.getX());
+            touchPosition.setY((int)event.getY());
+
+        }
+        if(action == MotionEvent.ACTION_MOVE){
+
+            drawer.offsetX = ((int) event.getX() - touchPosition.getX());
+            drawer.offsetY = ((int) event.getY() - touchPosition.getY());
+
+        }
+
+
+        return super.onTouchEvent(event);
+    }
+
 
 }
