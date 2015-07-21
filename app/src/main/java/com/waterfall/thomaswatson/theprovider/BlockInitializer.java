b66e2bce8a86ef34f3a7a179d8f4eb4e7fd947ac
Thomas Watson <thomas.sm.watson@gmail.com>
2015-07-21 07:26:02 +0100

    Finish generateSquare algorithm, TODO: create generateRandomArea

--- app/src/main/java/com/waterfall/thomaswatson/theprovider/BlockInitializer.java
+++ app/src/main/java/com/waterfall/thomaswatson/theprovider/BlockInitializer.java
@@ -12,46 +12,55 @@ import java.util.ArrayList;
  */
 public class BlockInitializer {
 
-    private Context context;
-    BlockFactory factory;
+        private Context context;
+        BlockFactory factory;
 
 
-    public BlockInitializer(Context context){
-        prepareVariablesForGeneration();
-        this.context = context;
-    }
-
-    //return an ArrayList of positions which have available Blocks
-    public static  ArrayList<Block> getAvailableBlocks(ArrayList<Block> blocks){
-        ArrayList<Block> availableBlocks = new ArrayList<Block>();
-
-        //loop through the blocks and check the type...
-        for(Block block:blocks){
-            switch(block.BLOCK_TYPE){
-                case BlockType.GRASS_BLOCK:
-                    availableBlocks.add(block);
-                    break;
-            }
+        public BlockInitializer(Context context){
+            prepareVariablesForGeneration();
+            this.context = context;
         }
-        return availableBlocks;
-    }
 
-    private void prepareVariablesForGeneration(){
-        factory = new BlockFactory();
-    }
+        //return an ArrayList of positions which have available Blocks
+        public static  ArrayList<Block> getAvailableBlocks(ArrayList<Block> blocks){
+            ArrayList<Block> availableBlocks = new ArrayList<Block>();
 
-    //generates a square patch of grass blocks at pos x,y. with the radius given
-    public ArrayList<Block> generateSquare(ArrayList<Block> blocks, int xPos, int yPos, int radius){
-        ArrayList<Block> generatedBlocks = new ArrayList<Block>();
-        for(Block block :blocks){
-            if (block.getPosition().<Integer>isGraterThan(xPos,yPos) && block.getPosition().<Integer>isLessThan(xPos+radius,yPos+radius)){
-                Position<Integer> lastPos = block.getPosition();
-                block = factory.makeBlock(BlockType.GRASS_BLOCK, context);
-                block.setPosition(lastPos);
+            //loop through the blocks and check the type...
+            for(Block block:blocks){
+                switch(block.BLOCK_TYPE){
+                    case BlockType.GRASS_BLOCK:
+                        availableBlocks.add(block);
+                        break;
+                }
             }
-            generatedBlocks.add(block);
+            return availableBlocks;
         }
-        return generatedBlocks;
+
+        private void prepareVariablesForGeneration(){
+            factory = new BlockFactory();
+        }
+
+        //generates a square patch of grass blocks at pos x,y. with the radius given
+        public ArrayList<Block> generateSquare(ArrayList<Block> blocks, int xPos, int yPos, int radius){
+            ArrayList<Block> generatedBlocks = new ArrayList<Block>();
+            for(Block block :blocks){
+                if (block.getPosition().getX() > xPos && block.getPosition().getX() < (xPos+radius) && block.getPosition().getY() > yPos && block.getPosition().getY() < (yPos+radius) ){
+                    Position<Integer> lastPos = block.getPosition();
+                    block = factory.makeBlock(BlockType.GRASS_BLOCK, context);
+                    block.setPosition(lastPos);
+                }
+                generatedBlocks.add(block);
+            }
+            return generatedBlocks;
+        }
+
+
+        //TODO: Generate a random area with the amount of blocks passed through
+        public ArrayList<Block> generateRandomArea(ArrayList<Block> blocks, Position<Integer> center, int amount) {
+
+            return null;
+        }
+
     }
 
 
